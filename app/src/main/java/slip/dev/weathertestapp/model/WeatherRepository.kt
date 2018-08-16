b69efa2a499b7c4151162fa433d2bd5153c4df5d
shaiytan <postscriptum105@gmail.com>
2018-08-16 21:52:50 +0300

    separate viewmodel from data source

---
+++ app/src/main/java/slip/dev/weathertestapp/model/WeatherRepository.kt
@@ -0,0 +1,39 @@
+package slip.dev.weathertestapp.model
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import slip.dev.weathertestapp.api.WeatherAPI
+
+class WeatherRepository {
+    private val weatherAPI = WeatherAPI.create()
+
+    private val weatherData = MutableLiveData<List<WeatherRecord>>()
+    fun getWeatherData(): LiveData<List<WeatherRecord>> = weatherData
+
+    private val statusData = MutableLiveData<String>().apply { value = "OK" }
+    fun getLoadingStatus(): LiveData<String> = statusData
+
+    fun loadForecast() {
+        statusData.value = "Loading"
+        weatherAPI.getForecast(47.83, 35.19).enqueue(object : Callback<ForecastResponse> {
+            override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
+                Log.e("LOADING", "LOADING FAILED", t)
+                statusData.value = "Failed to load forecast (${t.message})"
+            }
+
+            override fun onResponse(call: Call<ForecastResponse>, response: Response<ForecastResponse>) {
+                val forecast = response.body()?.forecast
+                if (response.isSuccessful && forecast != null) {
+                    weatherData.value = forecast
+                    statusData.value = "OK"
+                } else {
+                    statusData.value = "Invalid Response"
+                }
+            }
+        })
+    }
+}
--- app/src/main/java/slip/dev/weathertestapp/viewmodel/ForecastViewModel.kt
+++ app/src/main/java/slip/dev/weathertestapp/viewmodel/ForecastViewModel.kt
@@ -1,43 +1,12 @@
 package slip.dev.weathertestapp.viewmodel
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import android.util.Log
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import slip.dev.weathertestapp.api.WeatherAPI
-import slip.dev.weathertestapp.model.ForecastResponse
-import slip.dev.weathertestapp.model.WeatherRecord
+import slip.dev.weathertestapp.model.WeatherRepository
 
 class ForecastViewModel : ViewModel() {
-    private val weatherAPI = WeatherAPI.create()
+    private val repository = WeatherRepository()
 
-    private val weatherData = MutableLiveData<List<WeatherRecord>>()
-    fun getForecast(): LiveData<List<WeatherRecord>> = weatherData
-
-    private val statusData = MutableLiveData<String>().apply { value = "OK" }
-    fun getLoadingStatus(): LiveData<String> = statusData
-
-    fun loadForecast() {
-        statusData.value = "Loading"
-        weatherAPI.getForecast(47.83, 35.19).enqueue(object : Callback<ForecastResponse> {
-            override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
-                Log.e("LOADING", "LOADING FAILED", t)
-                statusData.value = "Failed to load forecast (${t.message})"
-                throw t
-            }
-
-            override fun onResponse(call: Call<ForecastResponse>, response: Response<ForecastResponse>) {
-                val forecast = response.body()?.forecast
-                if (response.isSuccessful && forecast != null) {
-                    weatherData.value = forecast
-                    statusData.value = "OK"
-                } else {
-                    statusData.value = "Invalid Response"
-                }
-            }
-        })
-    }
+    fun getForecast() = repository.getWeatherData()
+    fun getLoadingStatus() = repository.getLoadingStatus()
+    fun loadForecast() = repository.loadForecast()
 }
